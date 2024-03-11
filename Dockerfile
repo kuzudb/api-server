c9018b627813b48f7cff096a84d7258f4c9b81e8
囧囧 <liuc223@gmail.com>
2024-03-11 22:58:41 +0800

    First commit (#1)

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+FROM node:20-bookworm-slim
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Copy app
+COPY . /home/node/app
+
+# Make data and database directories
+RUN mkdir -p /database
+RUN mkdir -p /data
+RUN chown -R node:node /database
+RUN chown -R node:node /data
+
+# Install git, init submodules, minify, and remove git
+RUN cd /home/node/app &&\
+    apt-get update &&\
+    apt-get install -y git &&\
+    npm run clean &&\
+    npm run init-submodule &&\
+    npm run minify &&\
+    apt-get remove -y git &&\
+    apt-get autoremove -y &&\
+    apt-get clean &&\
+    chown -R node:node /home/node/app
+
+# Switch to node user
+USER node
+
+# Set working directory
+WORKDIR /home/node/app
+
+# Install dependencies and reduce size of kuzu node module
+RUN npm install &&\
+    rm -rf node_modules/kuzu/prebuilt node_modules/kuzu/kuzu-source
+
+# Expose port
+EXPOSE 8000
+
+# Set environment variables
+ENV NODE_ENV=production
+ENV PORT=8000
+ENV KUZU_PATH=/database
+
+# Run app
+ENTRYPOINT ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const process = require("process");
+const database = require("./explorer/src/server/utils/Database");
+const logger = require("./explorer/src/server/utils/Logger");
+
+const schema = require("./explorer/src/server/Schema");
+const cypher = require("./explorer/src/server/Cypher");
+
+let version;
+
+process.on("SIGINT", () => {
+  logger.info("SIGINT received, exiting");
+  process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM received, exiting");
+  process.exit(0);
+});
+
+const app = express();
+const PORT = 8000;
+
+const api = express.Router();
+api.use("/schema", schema);
+api.use("/cypher", cypher);
+
+app.use(express.json({ limit: "128mb" }));
+app.use("/", api);
+app.get("/version", (_, res) => {
+  res.send({ version: version });
+});
+app.get("/", (_, res) => {
+  res.send({ status: "OK" });
+});
+
+const conn = database.getConnection();
+conn
+  .query("CALL db_version() RETURN *;")
+  .then((res) => {
+    return res.getAll();
+  })
+  .then((res) => {
+    const row = res[0];
+    version = Object.values(row)[0];
+    logger.info("Version of Kùzu: " + version);
+    app.listen(PORT, () => {
+      logger.info("Deployed server started on port: " + PORT);
+    });
+  })
+  .catch((err) => {
+    logger.error("Error getting version of Kùzu: " + err);
+  })
+  .finally(() => {
+    database.releaseConnection(conn);
+  });
